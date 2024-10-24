2b6f7a5bb4f64896cb3f1ef40e93693c1b1c82bc
Fatima <antuzezfatima@gmail.com>
2024-10-24 16:35:30 +0200

    comentario de comprobacion

--- src/main/java/comunidad/comunidadVecinos/PrimaryController.java
+++ src/main/java/comunidad/comunidadVecinos/PrimaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
+//comentario para probar un push
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
