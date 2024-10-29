930455e9aaa5f1913ccdad1acf8f83954e3f8923
Antonio Estrada Blanco <antonioestradablanco@gmail.com>
2024-10-29 11:30:03 +0100

    Commit prueba Antonio Estrada

--- src/main/java/comunidad/comunidadVecinos/PrimaryController.java
+++ src/main/java/comunidad/comunidadVecinos/PrimaryController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 //comentario para probar un push
+	//ssssssss
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
