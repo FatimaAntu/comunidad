d0382b323d27313086eb4d4e18748482c64c4d58
Fatima <antuzezfatima@gmail.com>
2024-10-31 19:01:12 +0100

    he creado la interfaz del inicio de sesion y actualizado la de agregar usuario

---
+++ src/main/java/comunidad/comunidadVecinos/ControladorP_InicioSesion.java
@@ -0,0 +1,21 @@
+package comunidad.comunidadVecinos;
+
+public class ControladorP_InicioSesion {
+	   @FXML
+	    private TextField usernameField;  
+
+	    @FXML
+	    private TextField passwordField;  
+
+	    @FXML
+	    private Button guardarButton;  
+
+	    @FXML
+	    private ImageView flecha; 
+
+	    @FXML
+	    public void initialize() {
+	       
+	    }
+
+}
